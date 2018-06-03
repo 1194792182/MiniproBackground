e7003fac091d02e5dd0d88a3a9e70105ae49c3cc
1194792182 <1194792182@qq.com>
2018-06-03 18:58:42 +0800

    建立webapi

--- MiniProWebApi/Program.cs
+++ MiniProWebApi/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace Minipro
+namespace MiniProWebApi
 {
     public class Program
     {
